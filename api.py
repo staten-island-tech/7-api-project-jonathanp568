ad04d0f67b6e4edae53a3564a0bc4f91c2e4bc7d
jonathanp568 <jonathanpan0510@icloud.com>
2025-12-04 10:16:24 -0500

    Update api.py

--- api.py
+++ api.py
@@ -1,15 +1,17 @@
 import requests
+import tkinter as tk
 
 def getChar(char):
-    response = requests.get(f"https://rickandmortyapi.com/api/")
+    response = requests.get(f"https://rickandmortyapi.com/api/character")
     if response.status_code != 200:
         print("Error fetching data!")
         return None
-    
     data = response.json()
     return {
-        "name": data["name"],
-    }
-
-character = getChar("Jaguar")
+        "name": data["results"][0]["name"],
+        "status": data["results"][0]["status"],
+        "species": data["results"][0]["species"],
+        "origin": data["results"][0]["origin"]["name"]
+        }
+character = getChar("f")
 print(character)
